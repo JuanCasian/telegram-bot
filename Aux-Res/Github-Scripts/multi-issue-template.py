df365c5808ae44e504ebc1a83c3d766529d033c1
Juan Casian <juanpedrocasian@gmail.com>
2018-10-27 05:52:16 -0700

    Modifying multi issue script so that there is no need to change the code to send the issues

--- Aux-Res/Github-Scripts/multi-issue-template.py
+++ Aux-Res/Github-Scripts/multi-issue-template.py
@@ -8,12 +8,34 @@ contributors = {
         "Juan Pedro": "JuanCasian" }
 
 # This is the list of issues to edit for the automation
-issues = ["{} Github Basics", "{} Python Basics"]
+issues = [] 
+keep_asking = True
+while (keep_asking):
+    print ("Please enter the title to the issue:")
+    line = input()
+    issues.append("{} " + line)
+    print ("Do you want to add another issue? (yes/no)")
+    line = input()
+    if line != "yes":
+        keep_asking = False
+
 
 # From here DO NOT EDIT!
-
+print ("Please review the command that are going to me sent")
 command = "gh is --new --title \"{}\" -A {}"
 
+for issue in issues:
+    for key, value in contributors.items():
+        print(command.format(issue.format(key), value))
+
+print ("Is everything correct? (yes/no)")
+line = input()
+
+if (line != "yes"):
+    print ("Okay, cancelling commands")
+    exit()
+
+print("Sending commands")
 for issue in issues:
     for key, value in contributors.items():
         os.system(command.format(issue.format(key), value))
